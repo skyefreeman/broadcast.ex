85933300155a092457ef56f4bd2a14ebb7673ee4
skye <skyefreeman@icloud.com>
2024-10-24 17:17:41 -0400

    working bluesky posting.

--- broadcast.exs
+++ broadcast.exs
@@ -2,9 +2,6 @@
 
 Mix.install([:httpoison, :jason, :uuid])
 
-# DONE: externalize api key
-# DONE: externalize status
-
 defmodule Broadcast do
 
   def main(args \\ System.argv()) do
@@ -13,12 +10,15 @@ defmodule Broadcast do
       args,
       switches: [
 	mastodon_access_token: :string,
+	bluesky_handle: :string,
+	bluesky_password: :string,
 	status: :string,
 	debug: :boolean
       ]
     )
     
-    if opts[:mastodon_access_token] != nil and opts[:status] != nil do
+    if opts[:mastodon_access_token] != nil
+    and opts[:status] != nil do
       response = post_mastodon_status(
 	opts[:mastodon_access_token],
 	opts[:status]
@@ -30,22 +30,32 @@ defmodule Broadcast do
 	IO.inspect(response)
       end
     else
-      IO.puts("Skipping Mastodon. Missing options.")
+      IO.puts("Skipping Mastodon.")
     end
-
     
-    # TODO bluesky
-    post_bluesky_status()
-    #
-    # if opts[:mastodon_access_token] != nil and opts[:status] != nil do
-    #   post_mastodon_status(
-    # 	opts[:mastodon_access_token],
-    # 	opts[:status]
-    #   )
-    # else
-    #   IO.puts("Skipping Mastodon. Missing opts.")
-    # end
+    if opts[:bluesky_handle] != nil
+    and opts[:bluesky_password] != nil
+    and opts[:status] != nil do
+      response = post_bluesky_status(
+	opts[:bluesky_handle],
+	opts[:bluesky_password],
+	opts[:status]
+      )
 
+      case response do
+	{:ok, _} ->
+	  IO.puts("Bluesky: ok.")
+	{:error, response} ->
+	  IO.puts("Bluesky: #{response}")
+      end
+      
+      if opts[:debug] do
+	IO.inspect(response)
+      end
+    else
+      IO.puts("Skipping Bluesky.")
+    end
+    
     # TODO threads
     #
     # if opts[:mastodon_access_token] != nil and opts[:status] != nil do
@@ -58,9 +68,8 @@ defmodule Broadcast do
     # end
 
   end
-
+  
   def post_mastodon_status(access_token, status) do
-    
     base_url = "https://mastodon.social/api/v1/statuses"
     headers = [
       {"Authorization", "Bearer #{access_token}"},
@@ -69,19 +78,72 @@ defmodule Broadcast do
     ]
     params = %{"status" => status}
 
-    case HTTPoison.post(base_url, Jason.encode!(params), headers) do
+    post(base_url, params, headers)
+  end
+  
+  def post_bluesky_status(handle, password, status) do
+    # authenticate
+    response = post(
+      "https://bsky.social/xrpc/com.atproto.server.createSession",
+      %{
+	"identifier" => handle,
+	"password" => password
+      },
+      [{"Content-Type", "application/json"}]
+    )
+
+    case response do
+      {:ok, raw} ->
+	json = raw |> Jason.decode!
+	access_token = json |> Map.get("accessJwt")
+
+	# post the status
+	post(
+	  "https://bsky.social/xrpc/com.atproto.repo.createRecord",
+	  %{
+	    "repo" => handle,
+	    "collection" => "app.bsky.feed.post",
+	    "record" => %{
+	      "text" => status,
+	      "createdAt" => datetime_now()
+	    }
+	  },
+	  [
+	    {"Authorization", "Bearer #{access_token}"},
+	    {"Content-Type", "application/json"}
+	  ]
+	)
+
+      {:error, raw} ->
+	response
+    end
+  end
+
+  def post_threads_status() do
+    
+  end
+  
+  # helpers
+
+  def datetime_now do
+    (DateTime.utc_now
+    |> DateTime.to_iso8601
+    |> String.split(".")
+    |> List.first) <> "Z"
+  end
+  
+  def post(url, params, headers) do
+    case HTTPoison.post(url, Jason.encode!(params), headers) do
       {:ok, %HTTPoison.Response{status_code: 200, body: response_body}} ->
 	{:ok, response_body}
 
-      {:ok, %HTTPoison.Response{status_code: status_code}} ->
-	{:error, "Failed to post status. Status code: #{status_code}"}
+      {:ok, %HTTPoison.Response{status_code: status_code, body: response_body}} ->
+	{:error, "Unexpected Response:\n Status code: #{status_code}\n #{response_body}"}
 
       {:error, %HTTPoison.Error{reason: reason}} ->
 	{:error, "HTTP error: #{reason}"}
     end
   end
-
-  
 end
 
 Broadcast.main()
